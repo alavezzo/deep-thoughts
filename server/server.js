a0df8afb2cff65ea2d8d04693c4d693b4120c318
bowdenshot <anthonyelavezzo@gmail.com>
2021-11-03 20:36:59 -0700

    heroku issues

--- server/server.js
+++ server/server.js
@@ -1,36 +1,21 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 
-//import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const startServer = async () => {
-  // create a new Apollo server and pass in our schema data 
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
   });
 
-  // Start the Apollo server
-  await server.start();
-
-  // integrage our Apollo server with the Express application as middleware
-  server.applyMiddleware({ app });
-
-  // log where we can go to test our GQL API
-  console.log(`Use Graphql at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-// Initialize the Apollo server
-startServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
